22b0eef1a3365987f124106c612ce6563ca20604
Wasdns <952693358@qq.com>
2017-10-04 14:57:50 +0800

    update compile.sh for involving new files
    1.matchAlgorithmTools.cpp
    2.matchAlgorithm.cpp
    3.parser.cpp

--- Project/compile.sh
+++ Project/compile.sh
@@ -1,1 +1,2 @@
-gcc definition.cpp main.cpp jsonHandler.cpp outputJson.cpp -Wall -ljsoncpp -lstdc++ -o main
+gcc definition.cpp main.cpp jsonHandler.cpp outputJson.cpp matchAlgorithmTools.cpp matchAlgorithm.cpp parser.cpp -Wall -ljsoncpp -lstdc++ -o main
+ 
