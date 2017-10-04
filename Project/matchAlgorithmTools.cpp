c92ed5f25e0c5d65b43bbecad8a4a09f1abbaaaa
Wasdns <952693358@qq.com>
2017-10-04 14:54:46 +0800

    update Project: add algorithm tools and parser
    1.deliver the students to different classes;
    2.calculate the order among departments;
    3.parse time information from string.

--- Project/matchAlgorithmTools.cpp
+++ Project/matchAlgorithmTools.cpp
@@ -9,8 +9,26 @@ extern students student[310];
 extern departments department[25];
 extern addmitted addmitted_department[25];
 
+/*
+ *  Function Name: deliverStudent
+ *  deliver the students to two different classes
+ */ 
 void matchAlgorithmTools::deliverStudent() {
+	// for each of the students
+	for (int i = 0; i < 300; i++) {
+		// if the student does not choose any departments, 
+		// join he/she to the coldStudent
+		if (student[i].applications_department_number == 0) {
+			coldStudent[coldStudentNumber] = student[i];
+			coldStudentNumber++;
+			continue;
+		} 
 
+		// otherwise, the student has chosen some(>=1) departments,
+		// join he/she to the eagerStudent
+		eagerStudent[eagerStudentNumber] = student[i];
+		eagerStudentNumber++;
+	}
 }
 
 int matchAlgorithmTools::matchedLevelValue(int k, students student_instance, departments department_instance) {
@@ -19,7 +37,54 @@ int matchAlgorithmTools::matchedLevelValue(int k, students student_instance, dep
 	return 0;
 }
 
-void matchAlgorithmTools::calculateDepOrder() {
+struct department_sorted_t {
+	int index;
+	int score;
+};
 
+department_sorted_t department_sorted[21];
+
+bool cmp(department_sorted_t &department_instance1, department_sorted_t &department_instance2) {
+	return department_instance1.score > department_instance2.score;
 }
 
+/*
+ *  Function Name: calculateDepOrder
+ *  calculate the order among departments
+ *  based on studentWishes.
+ */
+void matchAlgorithmTools::calculateDepOrder() {
+	// the mechanism of sorting departments
+	// studentWishes[department_instance][0] = value => +5*value
+	// studentWishes[department_instance][1] = value => +4*value
+	// studentWishes[department_instance][2] = value => +3*value
+	// studentWishes[department_instance][3] = value => +2*value
+	// studentWishes[department_instance][4] = value => +1*value
+
+	// calculate the scores of every departments
+
+	// for each of the departments
+	for (int i = 0; i < 20; i++) {
+		// initial the department_sorted[i]
+		department_sorted[i].index = i;
+		department_sorted[i].score = 0;
+
+		int score = 0;
+		
+		// search studentWishes and calculate score
+		for (int j = 0; j < 5; j++) {
+			score += studentWishes[i][j]*(5-j);
+		}
+
+		// record in department_sorted[i]
+		department_sorted[i].score = score;
+	}
+
+	// sort the departments based on score
+	sort(department_sorted.begin(), department_sorted.end(), cmp);
+
+	// record the order among departments in order[20]
+	for (int i = 0; i < 20; i++) {
+		order[i] = department_sorted[i].index;
+	}
+}
--- Project/parser.cpp
+++ Project/parser.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <iostream>
+#include <sstream>
 #include "definition.h"
 #include "parser.h"
 using namespace std;
@@ -8,14 +9,132 @@ extern students student[310];
 extern departments department[25];
 extern addmitted addmitted_department[25];
 
+// string example: 'Wed.16:00~18:00'
+
+/*
+ *  Function Name: parseWeekday
+ *  parse the weekday(eg.Sunday:0, Monday:1, ...) from string
+ */
 int parser::parseWeekday(string free_time) {
-	return 0;
+	int weekday = -1;
+	string weekday_str = "";
+
+	// for each char in free_time
+	for (int i = 0; i < free_time.length(); i++) {
+		if (free_time[i] == '.') break;
+		// join the current char to weekday
+		weekday += free_time[i];
+	}
+
+	stringstream ss;
+	ss << weekday_str;
+	ss >> weekday;
+
+	return weekday;
 }
 
+/*
+ *  Function Name: parseStartTime
+ *  parse the start time(min) from string
+ */
 int parser::parseStartTime(string free_time) {
-	return 0;
+	int start_time = -1;
+	int start_time_hour = -1;
+	int start_time_minute = -1;
+	// is the point('.') occurred?
+	bool isPointOccurred = false;
+	// is the partition char(':') occurred?
+	bool isPartitionCharOccurred = false;
+	string start_time_str_prefix = "";
+	string start_time_str_suffix = "";
+
+	// for each char in free_time
+	for (int i = 0; i < free_time.length(); i++) {
+		// if the current char == '.', mark the isPointOccurred as true
+		if (free_time[i] == '.') {
+			isPointOccurred = true;
+			continue;
+		}
+
+		// if the point has not occured, continue
+		if (!isPointOccurred) continue;
+
+		// if the current char == '~', break
+		if (free_time[i] == '~') break; 
+
+		// if the current char == ':', mark the isPartitionCharOccurred as true
+		if (free_time[i] == ':') {
+			isPartitionCharOccurred = true;
+			continue;
+		}
+
+		// if the isPartitionCharOccurred == false, 
+		// join the char to start_time_str_prefix
+		if (!isPartitionCharOccurred) {
+			start_time_str_prefix += free_time[i];
+		} else { // otherwise, join to start_time_str_suffix
+			start_time_str_suffix += free_time[i];
+		}
+	}
+	
+	stringstream ss;
+	ss << start_time_str_prefix;
+	ss >> start_time_hour;
+	ss << start_time_str_suffix;
+	ss >> start_time_minute;
+
+	// calculate total minutes
+	start_time = start_time_hour*60+start_time_minute;
+	return start_time;
 }
 
+/*
+ *  Function Name: parseEndTime
+ *  parse end time(min) from string
+ */
 int parser::parseEndTime(string free_time) {
-	return 0;
+	int end_time = -1;
+	int end_time_hour = -1;
+	int end_time_minute = -1;
+	// is the '~' occurred?
+	bool isPointOccurred = false;
+	// is the partition char(':') occurred?
+	bool isPartitionCharOccurred = false;
+	string end_time_str_prefix = "";
+	string end_time_str_suffix = "";
+
+	for (int i = 0; i < free_time.length(); i++) {
+		// if the current char == '~', mark the isPointOccurred as true
+		if (free_time[i] == '~') {
+			isPointOccurred = true;
+			continue;
+		}
+		
+		// if '~' has not occured, continue
+		if (!isPointOccurred) continue;
+
+		// if the current char == ':', mark the isPartitionCharOccurred as true
+		if (free_time[i] == ':') {
+			isPartitionCharOccurred = true;
+			continue;
+		}
+
+		// if the isPartitionCharOccurred == false, 
+		// join the char to end_time_str_prefix
+		if (!isPartitionCharOccurred) {
+			end_time_str_prefix += free_time[i];
+		} else { // otherwise, join to end_time_str_suffix
+			end_time_str_suffix += free_time[i];
+		}
+	}
+
+	stringstream ss;
+	ss << end_time_str_prefix;
+	ss >> end_time_hour;
+	ss << end_time_str_suffix;
+	ss >> end_time_minute;
+
+	// calculate total minutes
+	end_time = end_time_hour*60+end_time_minute;
+	return end_time;
 }
